77dab860c67ac67622e699b97b60c049f445f502
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-11-25 13:15:00 +0100

    igv

--- my-app/src/routes/Header.svelte
+++ my-app/src/routes/Header.svelte
@@ -1,15 +1,9 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
 
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
